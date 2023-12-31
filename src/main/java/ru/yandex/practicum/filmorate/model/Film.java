a39d32c41a1f4acfeaf497deac4ed2ab4fb5786d
Elena <elena-bokhan@yandex.ru>
2023-12-31 19:50:10 +0500

    feat: added sql

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Builder;
 import lombok.Data;
-import lombok.NonNull;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
